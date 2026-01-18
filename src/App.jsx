0b9f1c3dfd113ff5f43fef29ce66c148b170907d
DroxSebastianMR <josrojmer@gmail.com>
2026-01-18 14:15:40 -0500

    commit-links

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 
 function App() {
-  // Simple path-based rendering so /shop loads the Shop page directly
-  const path = window.location.pathname || "/";
-  const isShop = path.startsWith("/shop");
-
-  if (isShop) {
-    // Render the shop page inside the MainLayout so it shares header/footer and theme
-    return (
-      <MainLayout>
-        <Shop />
-      </MainLayout>
-    );
-  }
-
   return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
